7bbb990300c5649fe1d87a8499217a7be90c59ef
Kiril Petkov <49308732+KirilPetkov@users.noreply.github.com>
2023-04-15 23:02:12 +0100

    Add files via upload

---
+++ 26. Storage.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+// Creating a contract
+contract Storage {
+
+    // State variable are permanently store on storage
+    string public greet = "Hello World!";
+}
